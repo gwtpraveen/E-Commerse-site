97f9ba2add7bd1d98be2b76eceb12a1db93e45b3
Praveen Thamindu <praveenthamindu@gmail.com>
2023-04-02 23:38:55 +0530

    fix padding mistake on input feilds

--- src/routes/Signup.jsx
+++ src/routes/Signup.jsx
@@ -9,9 +9,9 @@ export default function Signup() {
                 <h1 className="h1">Sign Up</h1>   
                 <form action="" method="post" className="form">
                     <input className="inputFeild" type="text" name="" id="" placeholder="userName"/>
-                    <input type="email" name="email" id="email" placeholder="E-mail"/>
+                    <input type="email" name="email" id="email" placeholder="E-mail" className="inputFeild"/>
                     <input className="inputFeild" type="password" name="" id="" placeholder="password"/>
-                    <input type="password" name="passwordRe" id="passwordRe" placeholder="confirm password"/>
+                    <input type="password" name="passwordRe" id="passwordRe" placeholder="confirm password" className="inputFeild"/>
                     <input className="loginBtn" type="submit" value="Sign Up" />
                     <Link to="/login" className="link">Already Have an Account?</Link>
                 </form>
